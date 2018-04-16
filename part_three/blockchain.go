f52aa962977cf88c141311325d485d6534157853
sunzhongyuan <sunzhongyuan@lvwan.com>
2018-04-17 00:07:22 +0800

    typo

--- part_three/blockchain.go
+++ part_three/blockchain.go
@@ -14,7 +14,7 @@ type Blockchain struct {
 	db  *bolt.DB
 }
 
-type BlockchainInterator struct {
+type BlockchainIterator struct {
 	currentHash []byte
 	db          *bolt.DB
 }
@@ -54,13 +54,13 @@ func (bc *Blockchain) AddBlock(data string) {
 	}
 }
 
-func (bc *Blockchain) Iterator() *BlockchainInterator {
-	bci := &BlockchainInterator{bc.tip, bc.db}
+func (bc *Blockchain) Iterator() *BlockchainIterator {
+	bci := &BlockchainIterator{bc.tip, bc.db}
 
 	return bci
 }
 
-func (i *BlockchainInterator) Next() *Block {
+func (i *BlockchainIterator) Next() *Block {
 	var block *Block
 
 	err := i.db.View(func(tx *bolt.Tx) error {
